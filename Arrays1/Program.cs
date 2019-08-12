c453c8a38783e041b4a74b23acd84eb441d15597
gmullins925 <50452654+gmullins925@users.noreply.github.com>
2019-08-12 10:15:25 -0500

    Commit

---
+++ Arrays1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string [] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+			Console.WriteLine(days.Length.ToString());
+
+
+			for (int i = 0; i < days.Length; i++)
+			{
+				Console.WriteLine(days[i]);
+			}
+
+			foreach (string varName in days)
+			{
+				Console.WriteLine("The day is : " + varName);
+			}
+			Console.ReadLine();
+
+
+		}
+	}
+}
